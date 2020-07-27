cb7aa8f4d3be846e7d18cfa5e6b9b0558ad650db
Scott M Stark <starksm64@gmail.com>
2020-07-27 06:09:04 -0500

    Shell script to create the OSX installer package using JDK14 jpackage

---
+++ javafx/build-readyubikey.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Shell script to create the OSX installer package using JDK14 jpackage
+JDK14=/Library/Java/JavaVirtualMachines/jdk-14.0.2.jdk/Contents/Home
+
+$JDK14/bin/jar cvef ReadYubikey ReadYubikey.jar -C target/test-classes ReadYubikey.class
+$JDK14/bin/jpackage --name ReadYubikey --input . --main-jar ReadYubikey.jar --main-class ReadYubikey
