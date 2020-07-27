c9dcf997ec080f0a5357d0bfc3623eb15e1904b2
Scott M Stark <starksm64@gmail.com>
2020-07-27 06:10:21 -0500

    Add location of source

--- javafx/build-readyubikey.sh
+++ javafx/build-readyubikey.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Shell script to create the OSX installer package using JDK14 jpackage
+# Source is in test/java/ReadYubikey.java
 JDK14=/Library/Java/JavaVirtualMachines/jdk-14.0.2.jdk/Contents/Home
 
 $JDK14/bin/jar cvef ReadYubikey ReadYubikey.jar -C target/test-classes ReadYubikey.class
